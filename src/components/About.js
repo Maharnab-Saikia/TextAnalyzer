0edf6136cd87ae7e47f56c87156c79ee75538579
Maharnab Saikia <61657669+Maharnab-Saikia@users.noreply.github.com>
2021-10-11 01:18:42 +0530

    update

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import Button from '@mui/material/Button';
+import CoffeeIcon from '@mui/icons-material/Coffee';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function About(props) {
-
-
 
     return (
         <div className={`container border border-${props.mode === 'dark' ? 'white' : 'black'} rounded p-2`} style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
             <div className="p-4 mb-3 rounded" style={{ background: props.mode === 'dark' ? '#333333' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }}>
-                <h1 className="fst-italic">About</h1>
-                <p className="mb-0">This text analysis tool provides information on the complexity of a text, It allows you to analyse a text by counting the number of sentences, words and characters present. It also provides statistical information on the repetition of phrases and keywords. Our online text analyser / word counter is easy to use and free of charge.</p>
+                <h2 className="fst-italic">About Website</h2>
+                <p className="mb-5">This text analysis tool provides information on the complexity of a text, It allows you to analyse a text by counting the number of sentences, words and characters present. It also provides statistical information on the repetition of phrases and keywords. Our online text analyser / word counter is easy to use and free of charge.</p>
+                <h2 className="fst-italic">About Developer</h2>
+                <p className="mb-5">Hi, I am <strong>Maharnab Saikia</strong> a Web Developer and a Programmer from Assam, India. I am
+                    currently learning MERN stack and Django framework. This is a Text Analyzer website made with React, this is my first react
+                    project.</p>
+                <h3 className="fst-italic">Thank you for visiting my website,</h3>
+                <p>I am still a student, so a coffee would come in handy.</p>
+
+                <Button variant="contained" href="https://www.buymeacoffee.com/Maharnab" className="m-2" style={{ background: '#FFDD00', color: 'black' }} startIcon={<CoffeeIcon />}>Buy me a Coffee</Button>
+
+                <Button variant="contained" href="https://github.com/Maharnab-Saikia/TextAnalyzer" className="m-2" style={{ background: '#24292F', color: 'white' }} startIcon={<GitHubIcon />}>Open Source</Button>
             </div>
 
         </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,4 +1,11 @@
 import React, { useState } from 'react'
+import Grid from '@mui/material/Grid';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Button from '@mui/material/Button';
+
 
 export default function TextForm(props) {
 
@@ -51,33 +58,42 @@ export default function TextForm(props) {
     const [text, setText] = useState("");
 
     return (
-        <>
+        <div>
             <div className="container" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h2 className="my-3">{props.heading}</h2>
                 <div className="mb-3">
                     <textarea className="form-control" value={text} onChange={handleOnChange} style={{ background: props.mode === 'dark' ? '#333333' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} id="myBox" rows="8" placeholder="Enter text here..."></textarea>
                 </div>
-                <button disabled={text.length === 0} className="btn btn-primary m-1" onClick={handleUpClick}>Convert to Uppercase</button>
-                <button disabled={text.length === 0} className="btn btn-primary m-1" onClick={handleLoClick}>Convert to Lowercase</button>
-                <button disabled={text.length === 0} className="btn btn-primary m-1" onClick={handleReverseClick}>Reverse Text</button>
-                <button disabled={text.length === 0} className="btn btn-primary m-1" onClick={handleRemovePuncClick}>Remove Punctuation</button>
-                <button disabled={text.length === 0} className="btn btn-primary m-1" onClick={handleRemoveClick}>Remove Extra Whitespaces</button>
-                <button disabled={text.length === 0} className="btn btn-primary m-1" onClick={handleCopyClick}>Copy Text</button>
-                <button disabled={text.length === 0} className="btn btn-primary m-1" onClick={handleClearClick}>Clear Text</button>
+                <Button variant="contained" className="m-1" disabled={text.length === 0} onClick={handleUpClick}>Convert to Uppercase</Button>
+                <Button variant="contained" className="m-1" disabled={text.length === 0} onClick={handleLoClick}>Convert to Lowercase</Button>
+                <Button variant="contained" className="m-1" disabled={text.length === 0} onClick={handleReverseClick}>Reverse Text</Button>
+                <Button variant="contained" className="m-1" disabled={text.length === 0} onClick={handleRemovePuncClick}>Remove Punctuation</Button>
+                <Button variant="contained" className="m-1" disabled={text.length === 0} onClick={handleRemoveClick}>Remove Extra Whitespaces</Button>
+                <Button variant="contained" className="m-1" disabled={text.length === 0} onClick={handleCopyClick}>Copy Text</Button>
+                <Button variant="contained" className="m-1" disabled={text.length === 0} onClick={handleClearClick}>Clear Text</Button>
             </div>
-
 
             <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h2>Summery:</h2>
-                <p className="form-control mb-1" style={{ background: props.mode === 'dark' ? '#333333' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }}>{text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} Words and {text.length} Characters.</p>
-                <p className="form-control" style={{ background: props.mode === 'dark' ? '#333333' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }}>{0.008 * text.split(' ').filter((element) => { return element.length !== 0 }).length} Minutes Readtime.</p>
+                <Grid id="list">
+                    <List>
+                        <ListItem>
+                            <ListItemText>{text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} Words and {text.length} Characters.</ListItemText>
+                        </ListItem>
+                        <Divider />
+                        <ListItem>
+                            <ListItemText>{0.008 * text.split(' ').filter((element) => { return element.length !== 0 }).length} Minutes Readtime.</ListItemText>
+                        </ListItem>
+                    </List>
+                </Grid>
             </div>
-
 
             <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h2>Preview:</h2>
-                <p className="form-control" style={{ background: props.mode === 'dark' ? '#333333' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }}>{text.length > 0 ? text : 'Nothing to preview!'}</p>
+                <p className="form-control p-3" style={{ background: props.mode === 'dark' ? '#333333' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }}>{text.length > 0 ? text : 'Nothing to preview!'}</p>
             </div>
-        </>
+        </div>
     )
 }
+
+//container
